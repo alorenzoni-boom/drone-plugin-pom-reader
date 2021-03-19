c93576f61036056a9f7cf433972d830925236757
Antonijo Lorenzoni <antonijo.lorenzoni@boom.co>
2021-03-19 11:26:03 +0100

    test drone

--- pom-parser.sh
+++ pom-parser.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 VERSION=$(echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:version/text()' | /usr/bin/xmllint --shell pom.xml | grep -v /)
 ARTIFACT_ID=$(echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:artifactId/text()' | /usr/bin/xmllint --shell pom.xml | grep -v /)
-echo POM_VERSION=$VERSION >> .env
-echo POM_ARTIFACT_ID=$ARTIFACT_ID >> .env
+echo CURRENT_POM_VERSION=$VERSION >> .env
+echo CURRENT_POM_ARTIFACT_ID=$ARTIFACT_ID >> .env
 echo $VERSION >> .tags
-echo $VERSION
-echo $ARTIFACT_ID
 sed -i "s/XXXXX/$VERSION/" Dockerfile
